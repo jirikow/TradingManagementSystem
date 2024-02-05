1fb554e872a0bd5c80f3010880325c1115098416
jirikow <149414743+jirikow@users.noreply.github.com>
2024-02-05 08:34:51 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+from tkinter import *
+from tkinter import ttk
+
+root = TK()
+root.geometry("1352x650x0x0")
+root.title("Vehicle Trading Management System")
+root.configure(background='black')
+
+root.mainloop()
+
