b79d898f16cc256ec8f4fb7049d0496e2a84b244
jirikow <jarolimek8@gmail.com>
2024-02-08 17:41:25 +0100

    dodelan reset vsech kolonek a okynek

--- main.py
+++ main.py
@@ -7,14 +7,14 @@ root.title("Vehicle Trading Management System")
 root.configure(background='black')
 
 
-Tops = Frame(root, width=1350, height=100, bd=4, relief="raised")
+Tops = Frame(root, width=1350, height=100, bd=6, relief="raised")
 Tops.pack(side=TOP)
 
 
-lblInfo = Label(Tops, font=('arial', 46, 'bold'), text="Vehicle Sales Trading Management System", bd=20, anchor='w')
+lblInfo = Label(Tops, font=('arial', 46, 'bold'), text="Vehicle Sales Trading Management System ", bd=20, anchor='w')
 lblInfo.grid(row=0, column=0)
 
-bottom = Frame(root, width=1350, height=600, bd=4, relief="raised")
+bottom = Frame(root, width=1350, height=600, bd=10, relief="raised")
 bottom .pack(side=BOTTOM)
 
 bottomLeft = Frame(bottom, width=1000, height=600, bd=2, relief="raised")
@@ -70,6 +70,16 @@ def Reset():
     Total.set("")
     txtReceipt.delete("1.0",END)
 
+    var1.set(0)
+    var2.set(0)
+    var3.set(0)
+    var4.set(0)
+    var5.set(0)
+    var6.set("")
+    var7.set("")
+    var8.set(0)
+    var9.set(0)
+
 #======================================= 1 ==========================================#
 CustomerName = StringVar()
 CustomerAddress = StringVar()
@@ -110,27 +120,38 @@ Customized.set("0")
 Leather.set("0")
 GPS.set("0")
 
-lblModified = Checkbutton(bottomLeftBottomL, font=('arial', 16, 'bold'), text="Modified", fg="black", width=20, bd=8, anchor="w")
+var1 = IntVar()
+var2 = IntVar()
+var3 = IntVar()
+var4 = IntVar()
+var5 = IntVar()
+
+lblModified = Checkbutton(bottomLeftBottomL, font=('arial', 16, 'bold'), text="Modified", fg="black", width=20, bd=8, anchor="w",
+                          onvalue=1, offvalue=0, variable=var1)
 lblModified.grid(row=0, column=0)
 txtModified = Entry(bottomLeftBottomL, font=('arial', 16, 'bold'), bd=2, width=14, bg="white", justify='left', textvariable=Modified)
 txtModified.grid(row=0, column=1)
 
-lblStereo = Checkbutton(bottomLeftBottomL, font=('arial', 16, 'bold'), text="Stereo System", fg="black", width=20, bd=8, anchor="w")
+lblStereo = Checkbutton(bottomLeftBottomL, font=('arial', 16, 'bold'), text="Stereo System", fg="black", width=20, bd=8, anchor="w",
+                        onvalue=1, offvalue=0, variable=var2)
 lblStereo.grid(row=1, column=0)
 txtStereo = Entry(bottomLeftBottomL, font=('arial', 16, 'bold'), bd=2, width=14, bg="white", justify='left', textvariable=Stereo)
 txtStereo.grid(row=1, column=1)
 
-lblLeather = Checkbutton(bottomLeftBottomL, font=('arial', 16, 'bold'), text="Leather Interior ", fg="black", width=20, bd=8, anchor="w")
+lblLeather = Checkbutton(bottomLeftBottomL, font=('arial', 16, 'bold'), text="Leather Interior ", fg="black", width=20, bd=8, anchor="w",
+                         onvalue=1, offvalue=0, variable=var3)
 lblLeather.grid(row=2, column=0)
 txtLeather = Entry(bottomLeftBottomL, font=('arial', 16, 'bold'), bd=2, width=14, bg="white", justify='left', textvariable=Leather)
 txtLeather.grid(row=2, column=1)
 
-lblCustomized = Checkbutton(bottomLeftBottomL, font=('arial', 16, 'bold'), text="Customized Details", fg="black", width=20, bd=8, anchor="w")
+lblCustomized = Checkbutton(bottomLeftBottomL, font=('arial', 16, 'bold'), text="Customized Details", fg="black", width=20, bd=8, anchor="w",
+                            onvalue=1, offvalue=0, variable=var4)
 lblCustomized.grid(row=3, column=0)
 txtCustomized = Entry(bottomLeftBottomL, font=('arial', 16, 'bold'), bd=2, width=14, bg="white", justify='left', textvariable=Customized)
 txtCustomized.grid(row=3, column=1)
 
-lblGPS = Checkbutton(bottomLeftBottomL, font=('arial', 16, 'bold'), text="GPS", fg="black", width=20, anchor="w")
+lblGPS = Checkbutton(bottomLeftBottomL, font=('arial', 16, 'bold'), text="GPS", fg="black", width=20, anchor="w",
+                     onvalue=1, offvalue=0, variable=var5)
 lblGPS.grid(row=4, column=0)
 txtGPS = Entry(bottomLeftBottomL, font=('arial', 16, 'bold'), bd=2, width=14, bg="white", justify='left', textvariable=GPS)
 txtGPS.grid(row=4, column=1)
@@ -141,10 +162,9 @@ btnReceipt = Button(bottomLeftBottomL,pady=8, bd=2, fg="black", font=('arial', 1
 
 #======================================= 2 ==========================================#
 
-var1 = StringVar()
-var2 = StringVar()
-var3 = StringVar()
-var4 = StringVar()
+var6 = StringVar()
+var7 = StringVar()
+
 CostofCar = StringVar()
 CarMileage = StringVar()
 
@@ -153,7 +173,7 @@ CarMileage.set("0")
 
 lblChooseaCar = Label(bottomLeftTopR, font=('arial', 16, 'bold'), text="Choose a Car", fg="black", width=13, bd=14, anchor="w")
 lblChooseaCar.grid(row=0, column=0)
-cboChooseaCar = ttk.Combobox(bottomLeftTopR, textvariable= var1, state='readonly', font=('arial', 20, 'bold'), width=12)
+cboChooseaCar = ttk.Combobox(bottomLeftTopR, textvariable= var6, state='readonly', font=('arial', 20, 'bold'), width=12)
 cboChooseaCar['value']=('','Lamborghini','Buggati Veyron','Rolls Royce','Aston Martin')
 cboChooseaCar.current(0)
 cboChooseaCar.grid(row=1, column=0)
@@ -164,7 +184,7 @@ txtCostofCar.grid(row=3, column=0)
 
 lblTradeInaCar = Label(bottomLeftTopR, font=('arial', 16, 'bold'), text="Trade In a Car", fg="black", width=13, bd=14, anchor="w")
 lblTradeInaCar.grid(row=0, column=0)
-cboTradeInaCar = ttk.Combobox(bottomLeftTopR, textvariable= var2, state='readonly', font=('arial', 20, 'bold'), width=12)
+cboTradeInaCar = ttk.Combobox(bottomLeftTopR, textvariable= var7, state='readonly', font=('arial', 20, 'bold'), width=12)
 cboTradeInaCar['value']=('','100-5000','5001-20000','20001-100000','100001-1000000')
 cboTradeInaCar.current(0)
 cboTradeInaCar.grid(row=1, column=1)
@@ -181,12 +201,17 @@ Tax = StringVar()
 SubTotal = StringVar()
 Total = StringVar()
 
-lblVAT = Checkbutton(bottomLeftBottomR, font=('arial', 16, 'bold'), text="VAT", fg="black", width=13, bd=8, anchor="w")
+var8 = IntVar()
+var9 = IntVar()
+
+lblVAT = Checkbutton(bottomLeftBottomR, font=('arial', 16, 'bold'), text="VAT", fg="black", width=13, bd=8, anchor="w",
+                     onvalue=1, offvalue=0, variable=var8)
 lblVAT.grid(row=0, column=0)
 txtVAT = Entry(bottomLeftBottomR, font=('arial', 16, 'bold'), bd=2, width=17, bg="white", justify='left', textvariable=VAT)
 txtVAT.grid(row=0, column=1)
 
-lblDiscount = Checkbutton(bottomLeftBottomR, font=('arial', 16, 'bold'), text="Discount", fg="black", width=13, bd=8, anchor='w')
+lblDiscount = Checkbutton(bottomLeftBottomR, font=('arial', 16, 'bold'), text="Discount", fg="black", width=13, bd=8, anchor='w',
+                          onvalue=1, offvalue=0, variable=var9)
 lblDiscount.grid(row=1, column=0)
 txtDiscount = Entry(bottomLeftBottomR, font=('arial', 16, 'bold'), bd=2, width=17, bg="white", justify='left', textvariable=Discount)
 txtDiscount.grid(row=1, column=1)
